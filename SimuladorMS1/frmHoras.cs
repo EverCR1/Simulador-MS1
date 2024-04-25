44217d2c3523af20d2b3d3aa64c8dc9fcfd3d610
EverCR1 <evermcr11@gmail.com>
2024-04-24 21:33:55 -0600

    Simulaciones Completadas

--- SimuladorMS1/frmHoras.cs
+++ SimuladorMS1/frmHoras.cs
@@ -19,34 +19,23 @@ namespace SimuladorMS1
         {
             InitializeComponent();
         }
-        public int getSelectedClientIdFromDetallesHora()
-        {
-            // Obtener la instancia actual del formulario frmHoras
-            //frmHoras frmHoras = Application.OpenForms.OfType<frmHoras>().FirstOrDefault();
-            int selectedRow = dataHoras.CurrentRow.Index;
-            int clientId = Convert.ToInt32(dataHoras.Rows[selectedRow].Cells[0].Value);
-            return clientId;
-            
 
-        }
-
-       
+        // Evento al presionar una celda de la tabla Hora
         private void dataHoras_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
             // Verificar si la celda clickeada está dentro de los límites del DataGridView
             if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
             {
-                frmHoras frmHoras = new frmHoras();
+                frmClientes frmClientes = new frmClientes();
 
                 // Obtener el valor de la primera columna (índice 0)
                 int idCliente = Convert.ToInt32(dataHoras.Rows[e.RowIndex].Cells[0].Value);
+                Formulas.idCliente = idCliente;
 
-                // Obtener el valor de la segunda columna (índice 1)
-                //int hora = Formulas.idHora;
+                frmClientes.dataClientes.DataSource = operaciones.getProductoCliente(Formulas.escenario, Formulas.idHora, idCliente);
+                frmClientes.Text = "Cliente " + idCliente;
 
-                frmHoras.dataHoras.DataSource = operaciones.getProductoCliente(Formulas.escenario, Formulas.idHora, idCliente);
-
-                frmHoras.ShowDialog();
+                frmClientes.ShowDialog();
             }
 
 
