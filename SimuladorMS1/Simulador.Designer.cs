818e2b9fbd29bb42b4da90703ac400eb097c3397
Yako02 <47304633+Yakore@users.noreply.github.com>
2024-04-28 19:44:45 -0600

    Hotfix Interfaz

--- SimuladorMS1/Simulador.Designer.cs
+++ SimuladorMS1/Simulador.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.txtMedia = new System.Windows.Forms.TextBox();
             this.btnSimular = new System.Windows.Forms.Button();
             this.dataGeneral = new System.Windows.Forms.DataGridView();
@@ -54,9 +56,10 @@
             // txtMedia
             // 
             this.txtMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMedia.Location = new System.Drawing.Point(21, 68);
+            this.txtMedia.Location = new System.Drawing.Point(28, 84);
+            this.txtMedia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMedia.Name = "txtMedia";
-            this.txtMedia.Size = new System.Drawing.Size(250, 26);
+            this.txtMedia.Size = new System.Drawing.Size(332, 30);
             this.txtMedia.TabIndex = 0;
             this.txtMedia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMedia_KeyPress);
             // 
@@ -64,9 +67,10 @@
             // 
             this.btnSimular.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnSimular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSimular.Location = new System.Drawing.Point(1033, 105);
+            this.btnSimular.Location = new System.Drawing.Point(1377, 129);
+            this.btnSimular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSimular.Name = "btnSimular";
-            this.btnSimular.Size = new System.Drawing.Size(118, 50);
+            this.btnSimular.Size = new System.Drawing.Size(157, 62);
             this.btnSimular.TabIndex = 1;
             this.btnSimular.Text = "Simular";
             this.btnSimular.UseVisualStyleBackColor = false;
@@ -78,12 +82,29 @@
             this.dataGeneral.AllowUserToDeleteRows = false;
             this.dataGeneral.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGeneral.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGeneral.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGeneral.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGeneral.Location = new System.Drawing.Point(71, 237);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGeneral.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGeneral.Location = new System.Drawing.Point(95, 292);
+            this.dataGeneral.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGeneral.Name = "dataGeneral";
             this.dataGeneral.ReadOnly = true;
             this.dataGeneral.RowHeadersWidth = 51;
-            this.dataGeneral.Size = new System.Drawing.Size(898, 432);
+            this.dataGeneral.Size = new System.Drawing.Size(1197, 532);
             this.dataGeneral.TabIndex = 2;
             this.dataGeneral.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGeneral_CellDoubleClick);
             // 
@@ -92,18 +113,20 @@
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel1.Controls.Add(this.txtMedia);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(71, 37);
+            this.panel1.Location = new System.Drawing.Point(95, 46);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(289, 118);
+            this.panel1.Size = new System.Drawing.Size(385, 145);
             this.panel1.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Location = new System.Drawing.Point(23, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(286, 20);
+            this.label2.Size = new System.Drawing.Size(347, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Promedio de Clientes por Hora (λ):";
             // 
@@ -111,9 +134,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(292, 20);
+            this.label1.Size = new System.Drawing.Size(354, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Promedio de Productos por Cliente:";
             // 
@@ -122,17 +146,19 @@
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel2.Controls.Add(this.txtProductosCliente);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(379, 37);
+            this.panel2.Location = new System.Drawing.Point(505, 46);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(339, 118);
+            this.panel2.Size = new System.Drawing.Size(452, 145);
             this.panel2.TabIndex = 4;
             // 
             // txtProductosCliente
             // 
             this.txtProductosCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProductosCliente.Location = new System.Drawing.Point(21, 67);
+            this.txtProductosCliente.Location = new System.Drawing.Point(28, 82);
+            this.txtProductosCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtProductosCliente.Name = "txtProductosCliente";
-            this.txtProductosCliente.Size = new System.Drawing.Size(298, 26);
+            this.txtProductosCliente.Size = new System.Drawing.Size(396, 30);
             this.txtProductosCliente.TabIndex = 0;
             this.txtProductosCliente.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtProductosCliente_KeyPress);
             // 
@@ -141,27 +167,30 @@
             this.panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.txtHoras);
-            this.panel3.Location = new System.Drawing.Point(742, 37);
+            this.panel3.Location = new System.Drawing.Point(989, 46);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(227, 118);
+            this.panel3.Size = new System.Drawing.Size(303, 145);
             this.panel3.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 15);
+            this.label3.Location = new System.Drawing.Point(23, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 20);
+            this.label3.Size = new System.Drawing.Size(173, 25);
             this.label3.TabIndex = 1;
             this.label3.Text = "Horas a Simular:";
             // 
             // txtHoras
             // 
             this.txtHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHoras.Location = new System.Drawing.Point(21, 68);
+            this.txtHoras.Location = new System.Drawing.Point(28, 84);
+            this.txtHoras.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtHoras.Name = "txtHoras";
-            this.txtHoras.Size = new System.Drawing.Size(184, 26);
+            this.txtHoras.Size = new System.Drawing.Size(244, 30);
             this.txtHoras.TabIndex = 0;
             this.txtHoras.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHoras_KeyPress);
             // 
@@ -169,18 +198,19 @@
             // 
             this.labelEscenario.AutoSize = true;
             this.labelEscenario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelEscenario.Location = new System.Drawing.Point(473, 196);
+            this.labelEscenario.Location = new System.Drawing.Point(631, 241);
+            this.labelEscenario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelEscenario.Name = "labelEscenario";
-            this.labelEscenario.Size = new System.Drawing.Size(104, 20);
+            this.labelEscenario.Size = new System.Drawing.Size(126, 25);
             this.labelEscenario.TabIndex = 6;
             this.labelEscenario.Text = "Escenario 1";
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(292, 335);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar.Location = new System.Drawing.Point(389, 412);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(480, 115);
+            this.progressBar.Size = new System.Drawing.Size(640, 142);
             this.progressBar.TabIndex = 7;
             this.progressBar.Visible = false;
             // 
@@ -193,9 +223,10 @@
             // 
             this.btnPrev.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPrev.Location = new System.Drawing.Point(349, 705);
+            this.btnPrev.Location = new System.Drawing.Point(465, 868);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrev.Name = "btnPrev";
-            this.btnPrev.Size = new System.Drawing.Size(118, 50);
+            this.btnPrev.Size = new System.Drawing.Size(157, 62);
             this.btnPrev.TabIndex = 8;
             this.btnPrev.Text = "Anterior";
             this.btnPrev.UseVisualStyleBackColor = false;
@@ -205,9 +236,10 @@
             // 
             this.btnNext.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNext.Location = new System.Drawing.Point(526, 705);
+            this.btnNext.Location = new System.Drawing.Point(701, 868);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(118, 50);
+            this.btnNext.Size = new System.Drawing.Size(157, 62);
             this.btnNext.TabIndex = 9;
             this.btnNext.Text = "Siguiente";
             this.btnNext.UseVisualStyleBackColor = false;
@@ -215,10 +247,10 @@
             // 
             // Simulador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1213, 895);
+            this.ClientSize = new System.Drawing.Size(1617, 1102);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.progressBar);
@@ -229,6 +261,7 @@
             this.Controls.Add(this.dataGeneral);
             this.Controls.Add(this.btnSimular);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Simulador";
             this.Text = "Simulador";
             ((System.ComponentModel.ISupportInitialize)(this.dataGeneral)).EndInit();
--- SimuladorMS1/frmClientes.Designer.cs
+++ SimuladorMS1/frmClientes.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataClientes = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataClientes)).BeginInit();
             this.SuspendLayout();
@@ -38,23 +40,39 @@
             this.dataClientes.AllowUserToDeleteRows = false;
             this.dataClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataClientes.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataClientes.Location = new System.Drawing.Point(52, 50);
-            this.dataClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataClientes.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataClientes.Location = new System.Drawing.Point(69, 62);
+            this.dataClientes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataClientes.Name = "dataClientes";
             this.dataClientes.ReadOnly = true;
             this.dataClientes.RowHeadersWidth = 51;
             this.dataClientes.RowTemplate.Height = 24;
-            this.dataClientes.Size = new System.Drawing.Size(495, 267);
+            this.dataClientes.Size = new System.Drawing.Size(660, 329);
             this.dataClientes.TabIndex = 1;
             // 
             // frmClientes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dataClientes);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmClientes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cliente";
